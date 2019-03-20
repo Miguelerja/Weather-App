fd61f5895d0922b2c18d68f007e21ef270194e9a
Miguelerja <miguelerja@gmail.com>
2019-03-20 14:16:26 +0100

    add dynamic weather icon

--- src/components/currentWeather/CurrentWeather.js
+++ src/components/currentWeather/CurrentWeather.js
@@ -3,6 +3,59 @@ import React, { Component } from 'react'
 export default class CurrentWeather extends Component {
   state = {
     celsius: false,
+    image: null,
+  };
+
+  setIcon = (weather) => {
+    switch(weather) {
+      case 'Thunderstorm':
+        this.setState({
+          image: '/images/storm.png',
+        })
+        break;
+      case 'Drizzle':
+        this.setState({
+          image: '/images/Drizzle.png',
+        })
+        break;
+      case 'Rain':
+        this.setState({
+          image: '/images/rain.png',
+        })
+        break;
+      case 'Snow':
+        this.setState({
+          image: '/images/snow.png',
+        })
+        break;
+      case 'Clear':
+        this.setState({
+          image: '/images/sunny.png',
+        })
+        break;
+      case 'Fog':
+        this.setState({
+          image: '/images/fog.png',
+        })
+        break;
+      case 'Tornado':
+        this.setState({
+          image: '/images/tornado.png',
+        })
+        break;
+      case 'Clouds':
+        this.setState({
+          image: '/images/cloudy.png',
+        })
+        break;
+      default:
+        return null;
+    };
+  };
+
+  componentDidMount () {
+    const { weather } = this.props;
+    this.setIcon(weather);
   };
 
   handleClick = () => {
@@ -12,8 +65,8 @@ export default class CurrentWeather extends Component {
   };
 
   render() {
-    const { temp, clouds, wind, image, weather, description } = this.props;
-    const { celsius } = this.state;
+    const { temp, clouds, wind, weather, description } = this.props;
+    const { celsius, image } = this.state;
     return (
       <div>
         <img src={image} alt={weather} />
