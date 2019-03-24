de79de032f331bb8f49ebdec4054ab7f21c2a358
Miguelerja <miguelerja@gmail.com>
2019-03-24 12:47:36 +0100

    Refactor form component

--- src/components/citySearch/CitySearch.js
+++ src/components/citySearch/CitySearch.js
@@ -1,4 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Link from 'react-router-dom';
+import PropTypes from 'prop-types';
+import './citySearch.css';
+import Weather from '../weather/Weather';
+
+const Form = (props) => {
+  const { handleSearchInput, name, buttonClick } = props;
+
+  const handleChange = (event) => {
+    handleSearchInput(event);
+  };
+
+  const handleClick = () => buttonClick();
+
+  return (
+    <form className='search-form'>
+      <div className='search-form-container'>
+        <input 
+          className='input'
+          type="text" 
+          onChange={handleChange}
+          name='city'
+          value={name} 
+          placeholder='Barcelona, ES' />
+        <button
+          className='btn'
+          onClick={handleClick}>
+          Get Weather
+        </button>
+      </div>
+    </form>
+  )
+};
+
+Form.propTypes = {
+  handleSearchInput: PropTypes.func.isRequired,
+  buttonClick: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+};
 
 export default class CitySearch extends Component {
   state = {
@@ -11,29 +50,27 @@ export default class CitySearch extends Component {
     });
   };
 
-  handleClick = () => {
+  handleClick = (event) => {
+    event.preventDefault();
     this.setState({
       clicked: !this.state.clicked,
     });
   }
   
   render() {
-    const { clicked } = this.state;
+    const { clicked, name } = this.state;
     return (
-      <form>
-        <input 
-          className='input'
-          type="text" 
-          onChange={this.handleChange}
-          name='city'
-          value={this.state.name} 
-          placeholder='Barcelona, ES' />
-        <button
-          className='btn'
-          onClick={this.handleClick}>
-          Get Weather
-        </button>
-      </form>
+      <>
+        {(clicked) ?
+          <Weather />
+          :
+          <Form 
+            handleSearchInput={this.handleChange}
+            buttonClick={this.handleClick}
+            name={name}
+          />
+        }
+      </>
     )
   }
 }
